824757e39c5cb0feaaf9d21873233ea6146908b3
xudusheng <597132100@qq.com>
2016-11-25 16:40:32 +0800

    xxxxxxx
    Signed-off-by: xudusheng <597132100@qq.com>

---
+++ Example/Pods/Target Support Files/XDSKKK/XDSKKK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XDSKKK : NSObject
+@end
+@implementation PodsDummy_XDSKKK
+@end
---
+++ Example/Pods/Target Support Files/XDSKKK/XDSKKK-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "UIViewController+HideNavigationBar.h"
+
+FOUNDATION_EXPORT double XDSKKKVersionNumber;
+FOUNDATION_EXPORT const unsigned char XDSKKKVersionString[];
+
